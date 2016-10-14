057f1ee05a61bd30cece87faaf1a9aca8d16d14c
Lucy <lucykang@nyu.edu>
2016-10-14 01:51:19 -0700

    update

--- debug/rotate/js/script.js
+++ debug/rotate/js/script.js
@@ -3,17 +3,6 @@
 // });
 
 
-  if ($(window).width() <= 588) {
-    $("#flogo2").hide();
-};
-
-  if ($(window).width() <= 500) {
-    $("#ver").hide();
-    $(".btn").width(135);
-    $(".dropdown-menu").width(139);
- 
-};
-
 
 var layer = L.tileLayer('https://api.mapbox.com/styles/v1/livenlulu/ciu0azvas00322in5xzze3u48/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGl2ZW5sdWx1IiwiYSI6ImNpZ3h0ZzltbzB1cTQ0cG0zamthcno1dmwifQ.vZrmbXCCq15ZVuF6g6vhkA',{
     attribution: ''
